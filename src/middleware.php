1b7a340ff39e79c2e2b75afeb410b87fa9246041
Dedy Surya Prihantoro <44486274+dedysurya13@users.noreply.github.com>
2021-04-08 11:25:52 +0700

    Initial commit

---
+++ src/middleware.php
@@ -0,0 +1,36 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    // e.g: $app->add(new \Slim\Csrf\Guard);
+
+    //validasi api key
+    $app->add(function ($request, $response, $next){
+        $error = NULL;
+        if($request->getHeader('apiKey')){
+            $key = $request->getHeader('apiKey')[0];
+            try{
+                $sql = "SELECT * FROM aset_api where api_key=:apiKey";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindParam("apiKey", $key);
+                $stmt->execute();
+                $result = $stmt->fetchObject();
+                if(!$result){
+                    //Api Key tidak ada
+                    $error = 'Unidentified Key';
+                }
+            }catch(PDOException $e){
+                $error = array('error'=>$e->getMessage());
+            }
+        }else{
+            $error = 'Missing Key';
+        }
+
+        if($error){
+            return $response->withJson(array('error'=>$error),401);
+        }
+        
+        return $next($request, $response);
+    });
+};
---
+++ src/routes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'index.phtml', $args);
+    });
+
+
+    $app->get("/kategori/{kode_kategori}", function(Request $request, Response $response, array $args){
+        $kode_kategori = trim(strip_tag($args['kode_kategori']));
+        $sql = "SELECT kode_kategori, nama_kategori FROM aset_kategori_aset WHERE kode_kategori=:kode_kategori";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam("kode_kategori", $kode_kategori);
+        $stmt->execute();
+        $mainCount = $stmt->rowCount();
+        $result = $stmt->fetchObject();
+        if($mainCount==0){
+            return $this->response->withJson(['status' => 'error', 'message' => 'no result data'], 200);
+        }
+        return $response->withJson(['status' => 'success', 'data' => $result], 200);
+    });
+};
