cbc3ff67b6ab81c7e0f476276c50a9bf9883ac1f
Dedy Surya Prihantoro <44486274+dedysurya13@users.noreply.github.com>
2021-04-08 17:18:55 +0700

    API Auth, fix landing page handler

--- src/routes.php
+++ src/routes.php
@@ -7,17 +7,52 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
 
-        // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
-    });
+    //validasi api key
+    $cekAPIKey = function($request, $response, $next){
+        $error = null;
+        if($request->getHeader('ApiKey')){
+            $key = $request->getHeader('ApiKey')[0];
+            try{
+                $sql  = "SELECT * FROM aset_api WHERE api_key = :ApiKey";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindParam("ApiKey", $key);
+                $stmt->execute();
+                $result = $stmt->fetchObject();    
+                if(!$result){
+                //Api key tidak ada
+                $error = 'Unidentified key';
+                }
+            }catch(PDOException $e){
+                $error = array('error'=>$e->getMessage());
+            }
+        }else{
+            $error = 'Missing key';
+        }
+        if($error){
+            return $response->withJson(array('error'=>$error),401);
+        }
+        return $next($request, $response);
+    };
 
 
+
+    // //katgeori -semua kategori
+    $app->get('/kategori', function(Request $request, Response $response, array $args){
+        $sql = "SELECT kode_kategori, nama_kategori FROM aset_kategori_aset";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $mainCount = $stmt->rowCount();
+        $result = $stmt->fetchAll();
+        if($mainCount==0){
+            return $this->response->withJson(['status' => 'error', 'message' => 'no result data'], 200);
+        };
+        return $response->withJson(['status' => 'success', 'data' => $result], 200);
+    })->add($cekAPIKey);
+
+    // //kategori - satu kategori
     $app->get("/kategori/{kode_kategori}", function(Request $request, Response $response, array $args){
-        $kode_kategori = trim(strip_tag($args['kode_kategori']));
+        $kode_kategori = trim(strip_tags($args['kode_kategori']));
         $sql = "SELECT kode_kategori, nama_kategori FROM aset_kategori_aset WHERE kode_kategori=:kode_kategori";
         $stmt = $this->db->prepare($sql);
         $stmt->bindParam("kode_kategori", $kode_kategori);
@@ -26,7 +61,17 @@ return function (App $app) {
         $result = $stmt->fetchObject();
         if($mainCount==0){
             return $this->response->withJson(['status' => 'error', 'message' => 'no result data'], 200);
-        }
+        };
         return $response->withJson(['status' => 'success', 'data' => $result], 200);
+    })->add($cekAPIKey);
+
+    
+    //tampilan default
+    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
 };
