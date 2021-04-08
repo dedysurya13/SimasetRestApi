4793a622fbcfadd58f366fcb9bda751f441a9502
Dedy Surya Prihantoro <44486274+dedysurya13@users.noreply.github.com>
2021-04-08 17:18:13 +0700

    Pindah auth ke Routes

--- src/middleware.php
+++ src/middleware.php
@@ -6,31 +6,31 @@ return function (App $app) {
     // e.g: $app->add(new \Slim\Csrf\Guard);
 
     //validasi api key
-    $app->add(function ($request, $response, $next){
-        $error = NULL;
-        if($request->getHeader('apiKey')){
-            $key = $request->getHeader('apiKey')[0];
-            try{
-                $sql = "SELECT * FROM aset_api where api_key=:apiKey";
-                $stmt = $this->db->prepare($sql);
-                $stmt->bindParam("apiKey", $key);
-                $stmt->execute();
-                $result = $stmt->fetchObject();
-                if(!$result){
-                    //Api Key tidak ada
-                    $error = 'Unidentified Key';
-                }
-            }catch(PDOException $e){
-                $error = array('error'=>$e->getMessage());
-            }
-        }else{
-            $error = 'Missing Key';
-        }
+    // $app->add(function ($request, $response, $next){
+    //     $error = NULL;
+    //     if($request->getHeader('apiKey')){
+    //         $key = $request->getHeader('apiKey')[0];
+    //         try{
+    //             $sql = "SELECT * FROM aset_api where api_key=:apiKey";
+    //             $stmt = $this->db->prepare($sql);
+    //             $stmt->bindParam("apiKey", $key);
+    //             $stmt->execute();
+    //             $result = $stmt->fetchObject();
+    //             if(!$result){
+    //                 //Api Key tidak ada
+    //                 $error = 'Unidentified Key';
+    //             }
+    //         }catch(PDOException $e){
+    //             $error = array('error'=>$e->getMessage());
+    //         }
+    //     }else{
+    //         $error = 'Missing Key';
+    //     }
 
-        if($error){
-            return $response->withJson(array('error'=>$error),401);
-        }
+    //     if($error){
+    //         return $response->withJson(array('error'=>$error),401);
+    //     }
         
-        return $next($request, $response);
-    });
+    //     return $next($request, $response);
+    // });
 };
